b728d4d1ec8bc5d63f4b51763b2c2b41a0fedd71
Lnbsingson <lowellnathanielsingson@gmail.com>
2021-05-15 20:15:29 +0800

    Display Completion

--- Bibs/Display.cs
+++ Bibs/Display.cs
@@ -6,5 +6,21 @@ namespace Bibs
 {
     class Display
     {
+        private Display[] RGBD;
+        public Display(int length,int pixels, params int[] RGB)
+        {
+            RGBD = new Display[length];
+            for(int i = 0; i < RGB.Length; i++)
+            {
+                RGBD[i] = new Display(RGB[i], length);
+            }
+
+        }
+        public Display[] GetDisplays
+        {
+            get { return RGBD; }
+        }
+      
+        
     }
 }
