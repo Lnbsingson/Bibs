9228a4557f91d8282e8e455fbcccc57bdde61fbe
Lnbsingson <lowellnathanielsingson@gmail.com>
2021-05-15 19:39:28 +0800

    Add project files.

---
+++ Bibs/GSM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bibs
+{
+    class GSM
+    {
+        public string Model;
+        public string Manufacturer;
+        public double Price;
+        public string Owner;
+        public GSM()
+        {
+            this.Model = null;
+            this.Manufacturer = null;
+            this.Price = 0;
+            this.Owner = null;
+
+        }
+        public GSM(string model)
+        {
+            this.Model = model;
+            this.Manufacturer = null;
+            this.Price = 0;
+            this.Owner = null;
+        }
+
+        public GSM(string model, string manufacturer)
+        {
+            this.Model = model;
+            this.Manufacturer = manufacturer;
+            this.Price = 0;
+            this.Owner = null;
+        }
+
+        public GSM(string model, string manufacturer, double price, string owner)
+        {
+            this.Model = model;
+            this.Manufacturer = manufacturer;
+            this.Price = price;
+            this.Owner = owner;
+        }
+
+        public string getModel
+        {
+            get { return Model; }
+            set { Model = value; }
+        }
+
+        public string getmanufacturer
+        {
+            get { return Manufacturer; }
+            set { Manufacturer = value; }
+        }
+
+        public double getprice
+        {
+            get { return Price; }
+            set { Price = value; }
+        }
+
+        public string getOwner
+        {
+            get { return Owner; }
+            set { Owner = value; }
+        }
+        public static string NokiaN95 = "NOKIA N95 Best phone UWU";    
+
+        public void DisInfo()
+        {
+            Console.WriteLine("Model: "+ Model);
+            Console.WriteLine("Manufacturer: " + Manufacturer);
+            Console.WriteLine("Price: " + Price);
+            Console.WriteLine("Owner: " + Owner);
+        }
+    }
+
+    
+}
---
+++ Bibs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GSM test = new GSM("Nokia", "Apple" , 190, "biboy");
+
+            test.DisInfo();
+            Console.WriteLine(test.getmanufacturer);
+           
+        }
+    }
+}
