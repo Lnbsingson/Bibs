2fcccf72ec2614548558f0419dd1c5e1fbbd0cb9
Lnbsingson <lowellnathanielsingson@gmail.com>
2021-05-15 21:46:37 +0800

    done final

--- Bibs/Program.cs
+++ Bibs/Program.cs
@@ -6,8 +6,10 @@ namespace Bibs
     {
         static void Main(string[] args)
         {
+            object[] instances = new object[10];
+            
             GSM test = new GSM("Nokia", "Apple" , 190, "biboy");
-
+            instances[0] = test;
             test.DisInfo();
             Console.WriteLine(test.getmanufacturer);
            
